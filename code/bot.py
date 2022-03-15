a652149363b161caf5e12323c4078cc4d358f622
EfiTower51 <90806205+EfiTower51@users.noreply.github.com>
2022-03-15 20:24:39 +0800

    Update bot.py

--- code/bot.py
+++ code/bot.py
@@ -1,44 +1,47 @@
+import os
 import discord
 from discord.ext import commands
-import os
-intents = discord.Intents.all()
 
+intents = discord.Intents.all()
 import json 
 with open('Dice\GitHub\code\setting.json',mode='r',encoding='UTF-8') as Jfile:
     Fjd = json.load(Jfile)
 
 
-bot = commands.Bot(command_prefix='5t-',intents=intents,debug_guilds=[int(Fjd['Tower'])])
+bot = discord.Bot(command_prefix='5t-',intents=intents,debug_guilds=[int(Fjd['Tower'])])
 
+master = ['WuYin悟音#5318','艾菲MK#4816']
 
 @bot.event
 #機器人之事件
 async def on_ready():
- print("Tower-sec online!")
+ print(f"{bot.user} online!")
 
 
-@bot.event
-async def on_member_join(member):
-    chaanel = bot.get_channel(865942368887767081)
-    await chaanel.send(f'{member}加入了伺服器 :server_icon:')
-    #await:協程功能綴首
-    print(f'{member} join!')
+#@bot.event
+#async def on_member_join(member):
+#    chaanel = bot.get_channel(865942368887767081)
+#    await chaanel.send(f'{member}加入了伺服器 :server_icon:')
+#    #await:協程功能綴首
+#    print(f'{member} join!')
 
 
-@bot.event
-async def on_member_remove(member):
-    chaanel = bot.get_channel(865942368887767081)
-    await chaanel.send(f'{member}離開了伺服器 :server_icon:')
-    print(f'{member} leave.')
+#@bot.event
+#async def on_member_remove(member):
+#    chaanel = bot.get_channel(865942368887767081)
+#    await chaanel.send(f'{member}離開了伺服器 :server_icon:')
+#    print(f'{member} leave.')
+
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(round(bot.latency*1000))
     print(f'pung!')
 
-@bot.command()
-async def sechelp(ctx):
-    await ctx.send("""
+
+@bot.slash_command(name="intro",description="關於機器人的介紹。")
+async def intro(ctx):
+    await ctx.respond("""
     屬於鐵塔的機器人助手！
     
     ◎Minecraft-TowerServer訊息機器人（偶爾出現）
@@ -50,16 +53,24 @@ async def sechelp(ctx):
     
     這裡什麼都沒有，或許之後會出現什麼吧。""")
 
+
 @bot.command()
 async def load(ctx,extension):
     bot.load_extension(f'{extension}')
     await ctx.send(f'Loaded {extension} Done.')
+    print(f'Loaded {extension}')
 
 @bot.event
 async def on_message(msg):
     if msg.content.startswith('http'):
         await msg.channel.send('沒事放連結幹嘛(´・ω・`)')
+
+@bot.event 
+async def on_message(msg):
+    call =['機器人助手','助手']
     
+    if msg.content in call and msg.author is master :
+        await msg.channel.send('主人叫我有什麼事嗎(｡•ㅅ•｡)♡')
 
 
 
